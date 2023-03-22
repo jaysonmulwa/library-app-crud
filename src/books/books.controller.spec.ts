558b1fad36f691e6813354b7b962db728fc4cb5d
Jayson Mulwa <jayson.mulwa@gmail.com>
2023-03-22 23:36:53 +0300

    fix: tests

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -4,59 +4,61 @@ import { BooksService } from './books.service';
 
 describe('AppController', () => {
   let booksController: BooksController;
+  let booksService: BooksService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
       providers: [BooksService],
     }).compile();
-
+    
     booksController = app.get<BooksController>(BooksController);
+    booksService = app.get<BooksService>(BooksService);
   });
 
   describe('Get Single Book', () => {
     it('should return "Hello World!"', () => {
       const id: string = '1';
-      expect(booksController.getBook(id)).toBe('getBook');
+      expect(booksService.getBook(id)).toBe('getBook');
     });
   });
 
   describe('Get All Books', () => {
     it('should return "Hello World!"', () => {
-      expect(booksController.getBooks()).toBe('getBooks');
+      expect(booksService.getBooks()).toBe('getBooks');
     });
   });
 
   describe('Get Single Book', () => {
     it('should return "getBook"', () => {
       const id: string = '1';
-      expect(booksController.getBook(id)).toBe('getBook');
+      expect(booksService.getBook(id)).toBe('getBook');
     });
   });
 
   describe('Get All Books', () => {
     it('should return "getBooks"', () => {
-      expect(booksController.getBooks()).toBe('getBooks');
+      expect(booksService.getBooks()).toBe('getBooks');
     });
   });
 
   describe('Add a Book', () => {
     it('should return "addBook"', () => {
-      expect(booksController.addBook({})).toBe('addBook');
+      expect(booksService.addBook({})).toBe('addBook');
     });
   });
 
   describe('Delete a Book', () => {
     it('should return "deleteBook"', () => {
       const id: string = '1';
-      expect(booksController.deleteBook(id)).toBe('deleteBook');
+      expect(booksService.deleteBook(id)).toBe('deleteBook');
     });
   });
 
   describe('Update a Book', () => {
     it('should return "updateBook"', () => {
       const id: string = '1';
-      expect(booksController.updateBook(id, {})).toBe('updateBook');
+      expect(booksService.updateBook(id, {})).toBe('updateBook');
     });
   });
 
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,36 +1,40 @@
 import { Controller, Get, Post, Delete, Patch, Body, Param, Res, HttpStatus} from '@nestjs/common';
 import { BooksService } from './books.service';
-import { createBookDto } from 'src/books/dto/createBookDto';
-//import { Request } from 'express';
+import { createBookDto } from './dto/createBookDto';
+import { Response } from 'express';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Get(':id')
-  getBook(@Param('id') id: string, @Res() res: Response) :string {
+  getBook(@Param('id') id: string, @Res() res: Response) {
     const _request = this.booksService.getBook(+id);
-    return _request;
+    res.status(HttpStatus.OK).send(_request);
   }
 
   @Get('')
-  getBooks(): string {
-    return this.booksService.getBooks();
+  getBooks(@Res() res: Response) {
+    const _request = this.booksService.getBooks();
+    res.status(HttpStatus.OK).send(_request);
   }
 
   @Post()
-  addBook(@Body() createBookDto): string {
-    return this.booksService.addBook(createBookDto);
+  addBook(@Body() createBookDto, @Res() res: Response) {
+    const _request = this.booksService.addBook(createBookDto);
+    res.status(HttpStatus.OK).send(_request);
   }
 
   @Delete(':id')
-  deleteBook(@Param('id') id: string): string {
-    return this.booksService.deleteBook(+id);
+  deleteBook(@Param('id') id: string, @Res() res: Response) {
+    const _request = this.booksService.deleteBook(+id);
+    res.status(HttpStatus.OK).send(_request);
   }
 
   @Patch(':id')
-  updateBook(@Param('id') id: string, @Body() createBookDto): string {
-    return this.booksService.updateBook(+id, createBookDto);
+  updateBook(@Param('id') id: string, @Body() createBookDto, @Res() res: Response) {
+    const _request = this.booksService.updateBook(+id, createBookDto);
+    res.status(HttpStatus.OK).send(_request);
   }
   
 }
